a536723d50c3c2f71b0c5cff3bc0990e345d6416
rahulreddy <rahulreddy062@gmail.com>
2019-12-02 22:19:15 -0600

    Add some changes

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Auxillary from '../../hoc/Auxillary';
+import classes from './Layout.css';
+const layout = (props) =>(
+    <Auxillary>
+    <div>Toolbar,SideDrawer,BackDrop</div>
+    <main className = {classes.Content}> 
+        {props.children}
+    </main>
+    </Auxillary>
+)
+export default layout; 
